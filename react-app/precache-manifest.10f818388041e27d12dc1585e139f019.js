cd4807a2d202b2e25e77ec3e0b6842cc98c03977
Agnas05 <agnasgreesma24@gmail.com>
2022-10-10 15:32:57 +0530

    buildfile

---
+++ react-app/precache-manifest.10f818388041e27d12dc1585e139f019.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2e3671354e678304b3d",
+    "url": "/FirstApp/react-app/static/css/main.c065d859.chunk.css"
+  },
+  {
+    "revision": "c2e3671354e678304b3d",
+    "url": "/FirstApp/react-app/static/js/main.c2e36713.chunk.js"
+  },
+  {
+    "revision": "1b7926b37cc8ed63247e",
+    "url": "/FirstApp/react-app/static/js/1.1b7926b3.chunk.js"
+  },
+  {
+    "revision": "f3653a436afa1c69290d",
+    "url": "/FirstApp/react-app/static/css/2.b2b00809.chunk.css"
+  },
+  {
+    "revision": "f3653a436afa1c69290d",
+    "url": "/FirstApp/react-app/static/js/2.f3653a43.chunk.js"
+  },
+  {
+    "revision": "fed33543db7bf4f0ccdc",
+    "url": "/FirstApp/react-app/static/js/runtime~main.fed33543.js"
+  },
+  {
+    "revision": "a756c7481ac215300f16108a8c27e6b9",
+    "url": "/FirstApp/react-app/static/media/register.a756c748.jpg"
+  },
+  {
+    "revision": "36ce4a157ead8391f6a656a9626d7693",
+    "url": "/FirstApp/react-app/static/media/rainy.36ce4a15.jpg"
+  },
+  {
+    "revision": "dcb002e89d067654b4db8f5556c1cfa4",
+    "url": "/FirstApp/react-app/static/media/sunny.dcb002e8.jpg"
+  },
+  {
+    "revision": "ac813d6b72683bf07fb18f7da672d46d",
+    "url": "/FirstApp/react-app/static/media/cloudy.ac813d6b.jpg"
+  },
+  {
+    "revision": "70de29522d07a061d5db8ba74c155872",
+    "url": "/FirstApp/react-app/static/media/welcome.70de2952.webp"
+  },
+  {
+    "revision": "9c4a48ef6bc9607b38d1edb0c5104e7f",
+    "url": "/FirstApp/react-app/static/media/bluepurple.9c4a48ef.png"
+  },
+  {
+    "revision": "5dd49c0f9abfab74c99b84a650929166",
+    "url": "/FirstApp/react-app/static/media/zuGOUB.5dd49c0f.jpg"
+  },
+  {
+    "revision": "903f5cda58599de771b56209c3cb7440",
+    "url": "/FirstApp/react-app/static/media/todo1.903f5cda.jpg"
+  },
+  {
+    "revision": "e96b5632d0d722574ab473c3c262f98f",
+    "url": "/FirstApp/react-app/static/media/purple.e96b5632.png"
+  },
+  {
+    "revision": "f553e32e147b66edc58a5af81a0c8e5b",
+    "url": "/FirstApp/react-app/static/media/blue.f553e32e.jpg"
+  },
+  {
+    "revision": "9ef204c52de0c685fa8604792a10302a",
+    "url": "/FirstApp/react-app/index.html"
+  }
+];
--- react-app/service-worker.js
+++ react-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37daa172112a9995e0da62fd1b751578.js"
+  "/FirstApp/react-app/precache-manifest.10f818388041e27d12dc1585e139f019.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/FirstApp/react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
